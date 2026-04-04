4c3bbaa82e8798b1e3baab33a3a546a55b74c99e
Ilyas <uncognic@1707070.xyz>
2026-04-03 21:50:47 -0400

    feat: emit assembly from BASIC

--- src/blackboxc/compiler.c
+++ src/blackboxc/compiler.c
@@ -11,18 +11,21 @@ int main(int argc, char *argv[])
 {
     if (argc > 1 && (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0))
     {
-        fprintf(stdout, "Usage: %s [-d, --debug] [-h, --help] input.bbx/.bbs <output.bcx>\n", argv[0]);
+        fprintf(stdout, "Usage: %s [-d, --debug] [-h, --help] [-a, --asm] input.bbx/.bbs <output.bcx>\n", argv[0]);
         return 1;
     }
 
     char *input_file = NULL;
     char *output_file = NULL;
-    int debug = 0;
+    uint8_t debug = 0;
+    uint8_t assembly = 0;
 
     for (int i = 1; i < argc; i++)
     {
         if (strcmp(argv[i], "-d") == 0 || strcmp(argv[i], "--debug") == 0)
             debug = 1;
+        else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--asm") == 0)
+            assembly = 1;
         else if (!input_file)
             input_file = argv[i];
         else if (!output_file)
@@ -36,11 +39,32 @@ int main(int argc, char *argv[])
 
     if (!input_file)
     {
-        fprintf(stderr, "Usage: %s [-d] input.bbx/.bbs <output.bcx>\n", argv[0]);
+        fprintf(stderr, "Usage: %s [-d, --debug] [-h, --help] [-a, --asm] input.bbx/.bbs <output.bcx>\n", argv[0]);
         return 1;
     }
 
-    if (!output_file)
+    char *asm_output = NULL;
+
+    if (assembly)
+    {
+        char *filename = strrchr(input_file, '\\');
+        if (!filename)
+            filename = strrchr(input_file, '/');
+        if (filename)
+            filename++;
+        else
+            filename = input_file;
+
+        asm_output = malloc(strlen(filename) + 5);
+        if (!asm_output)
+        {
+            perror("malloc");
+            return 1;
+        }
+        sprintf(asm_output, "%s.bbx", filename);
+        output_file = asm_output;
+    }
+    else if (!output_file)
     {
         printf("Output file not specified, defaulting to 'out.bcx'\n");
         output_file = "out.bcx";
@@ -50,6 +74,7 @@ int main(int argc, char *argv[])
     if (!in)
     {
         perror("fopen input");
+        free(asm_output);
         return 1;
     }
 
@@ -93,13 +118,21 @@ int main(int argc, char *argv[])
             printf("[DEBUG] Pathway: basic\n");
         }
         result = preprocess_basic(input_file, output_file, debug);
+
         if (result == 0)
             printf("BASIC preprocessing successful.\n");
-        
+
+        if (assembly)
+        {
+            free(asm_output);
+            return result;
+        }
+
         result = assemble_file(output_file, output_file, debug);
         if (result == 0)
             printf("Assembly successful.\n");
     }
 
+    free(asm_output);
     return result;
 }
