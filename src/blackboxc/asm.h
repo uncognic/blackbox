58351afb877309ea1a8649c98b326a4ee3b1b1cf
Ilyas <uncognic@1707070.xyz>
2026-04-04 21:47:30 -0400

    refactor: make headers more C++

--- src/blackboxc/asm.h
+++ src/blackboxc/asm.h
@@ -1,14 +1,2 @@
-#ifndef ASM_H
-#define ASM_H
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
+#pragma once
 int assemble_file(const char *filename, const char *output_file, int debug);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
--- src/data.h
+++ src/data.h
@@ -4,13 +4,7 @@
 #include <cstdio>
 #include <cstddef>
 #include <cctype>
-using std::FILE;
-
 #include "define.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
 
 void write_u32(FILE *out, uint32_t val);
 uint32_t read_u32(const uint8_t *data, size_t *pc);
@@ -21,7 +15,4 @@ uint64_t read_u64(const uint8_t *data, size_t *pc);
 void write_i64(FILE *out, int64_t val);
 int64_t read_i64(const uint8_t *data, size_t *pc);
 
-#ifdef __cplusplus
-}
-#endif
 
