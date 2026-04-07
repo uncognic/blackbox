7f85c3ae1dd7a43cebe1f06be8d15cc5f3e3108a
Ilyas <uncognic@1707070.xyz>
2026-04-07 17:24:00 +0000

    refacator: use std::string in data lookups

--- src/utils/symbol_table.cpp
+++ src/utils/symbol_table.cpp
@@ -8,23 +8,23 @@
 namespace blackbox {
 namespace tools {
 
-uint32_t find_label(const char* name, Label* labels, size_t count) {
+uint32_t find_label(const std::string& name, Label* labels, size_t count) {
     for (size_t i = 0; i < count; i++) {
-        if (strcmp(labels[i].name, name) == 0) {
+        if (name == labels[i].name) {
             return labels[i].addr;
         }
     }
-    fprintf(stderr, "Unknown label %s\n", name);
+    fprintf(stderr, "Unknown label %s\n", name.c_str());
     exit(1);
 }
 
-uint32_t find_data(const char* name, Data* data, size_t count) {
+uint32_t find_data(const std::string& name, Data* data, size_t count) {
     for (size_t i = 0; i < count; i++) {
-        if (equals_ci(data[i].name, name)) {
+        if (equals_ci(data[i].name, name.c_str())) {
             return i;
         }
     }
-    fprintf(stderr, "Error: undefined string constant '%s'\n", name);
+    fprintf(stderr, "Error: undefined string constant '%s'\n", name.c_str());
     exit(1);
 }
 
--- src/utils/symbol_table.hpp
+++ src/utils/symbol_table.hpp
@@ -3,12 +3,13 @@
 #include "../define.hpp"
 #include <cstddef>
 #include <cstdint>
+#include <string>
 
 namespace blackbox {
 namespace tools {
 
-uint32_t find_label(const char* name, Label* labels, size_t count);
-uint32_t find_data(const char* name, Data* data, size_t count);
+uint32_t find_label(const std::string& name, Label* labels, size_t count);
+uint32_t find_data(const std::string& name, Data* data, size_t count);
 
 } // namespace tools
 } // namespace blackbox
